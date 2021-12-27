db79bbf38cbd4bb735c64b112b940d7768ca2156
anshultyag <you@example.com>
2021-12-27 20:31:37 +0530

    Added: perfect number

---
+++ src/Uc_02_Perfect_Number.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Uc_02_Perfect_Number {
+    public static int perfectNumber(int num) {
+        int sum = 0;
+        int i = 1;
+        while (i <= num / 2) {
+            if (num % i == 0) {
+                sum = sum + i;
+            }
+            i++;
+        }
+        if (sum == num) {
+            System.out.println("This is a perfect number");
+        } else {
+            System.out.println("This is not a perfect number");
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter your number");
+        int num = obj.nextInt();
+
+        perfectNumber(num);
+    }
+}
